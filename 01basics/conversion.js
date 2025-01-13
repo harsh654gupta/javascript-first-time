acf6aa8d3cf180765b67b1a6788415ea2e81cfec
harsh654gupta <hg9335223025@gmail.com>
2025-01-13 21:00:01 +0000

    conversion part done

---
+++ 01basics/conversion.js
@@ -0,0 +1,28 @@
+let a="harsh"
+let b="33"
+// console.log(typeof (b))
+// console.log(typeof null)
+// console.log(typeof undefined)
+// let c=Number(b);
+// console.log(c);
+// console.log(typeof c)
+// -------------------------------------------------------------------------------------------
+// console.log(2**4);
+// console.log(+true);
+// console.log(+false);
+// console.log(2/3)
+const num=12345677889876n // to declare big int 
+//console.log(typeof num);
+let obj={
+    name:"harsh",
+    cat:"meow"
+}
+//console.log(obj)
+const myf= function()
+{
+    console.log("wer are in a functin ");
+}
+//myf();
+console.log(typeof obj)
+console.log(typeof myf)
+
