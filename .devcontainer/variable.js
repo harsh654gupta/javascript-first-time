65b1271c19726c1acca5e402e652ff4ede6f3f67
harsh654gupta <hg9335223025@gmail.com>
2025-01-13 20:01:00 +0000

    datatypes

---
+++ .devcontainer/variable.js
@@ -0,0 +1,12 @@
+console.log("harsh gupta")
+var ac=123;
+const acc=124;
+let name="papa"
+ city ="kamala";
+let mew;
+console.table([ac,acc,name,city,mew,145]);
+city="vimaal"
+console.log(city);
+// prefer not to use var variable in javascript 
+
+// if a variable only declared then its value is undefined
